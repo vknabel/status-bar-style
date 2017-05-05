8a021b2d34370403add593732fc675752e70d0a2
Valentin Knabel <dev@vknabel.com>
2017-05-06 00:23:16 +0200

    [chore] Code Coverage

--- karma.conf.js
+++ karma.conf.js
@@ -1,41 +1,49 @@
-module.exports = function(config) {
-    config.set({
+module.exports = function (config) {
+  config.set({
 
-        frameworks: ["jasmine", "karma-typescript"],
+    frameworks: ["jasmine", "karma-typescript"],
 
-        files: [
-            { pattern: "src/testing/base.spec.ts" },
-            { pattern: "src/**/*.+(ts|html)" }
-        ],
+    files: [
+      { pattern: "src/testing/base.spec.ts" },
+      { pattern: "src/**/*.+(ts|html)" }
+    ],
 
-        preprocessors: {
-            "**/*.ts": ["karma-typescript"]
-        },
+    preprocessors: {
+      "**/*.ts": ["karma-typescript"]
+    },
 
-        karmaTypescriptConfig: {
-            bundlerOptions: {
-                entrypoints: /\.spec\.ts$/,
-                transforms: [
-                    require("karma-typescript-angular2-transform"),
-                    require("karma-typescript-es6-transform")()
-                ]
-            },
-            compilerOptions: {
-                lib: ["ES2015", "DOM"]
-            },
-            coverageOptions: {
-                instrumentation: true
-            }
-        },
+    karmaTypescriptConfig: {
+      bundlerOptions: {
+        entrypoints: /\.spec\.ts$/,
+        transforms: [
+          require("karma-typescript-angular2-transform"),
+          require("karma-typescript-es6-transform")()
+        ]
+      },
+      compilerOptions: {
+        lib: ["ES2015", "DOM"]
+      },
+      coverageOptions: {
+        instrumentation: true
+      }
+    },
 
-        reporters: ["progress", "karma-typescript"],
+    reporters: ["progress", "karma-typescript"],
 
-        singleRun: process.env['CI']
-          ? true
-          : false,
+    singleRun: process.env['CI']
+      ? true
+      : false,
 
-        browsers: process.env['CI']
-          ? ["PhantomJS"]
-          : ["Chrome"]
-    });
+    browsers: process.env['CI']
+      ? ["PhantomJS"]
+      : ["Chrome"]
+  });
+  config.karmaTypescriptConfig.reports = {
+    json: "coverage",
+    "text-summary": "",
+    lcovonly: "coverage"
+  };
+  config.karmaTypescriptConfig.coverageOptions = {
+    exclude: /\.(d|spec|test|mock)\.ts$/i
+  };
 };
